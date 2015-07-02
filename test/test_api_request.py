b5114755faf56434081e1ac0206210529dae9dd7
TinEye dev <support@tineye.com>
2015-07-02 13:10:22 -0400

    Updating test_api_request file to work with API sandbox.

--- test/test_api_request.py
+++ test/test_api_request.py
@@ -5,20 +5,23 @@ test_api_request.py
 
 Test APIRequest class.
 
-Copyright (c) 2012 Idee Inc. All rights reserved worldwide.
+Copyright (c) 2015 Idée Inc. All rights reserved worldwide.
 """
 
-import time
 import unittest
 
 from pytineye.api_request import APIRequest
 from pytineye.exceptions import APIRequestError
 
+
 class TestAPIRequest(unittest.TestCase):
     """ Test APIRequest class. """
-    
+
     def setUp(self):
-        self.request = APIRequest('http://api.tineye.com/', 'public_key', 'private_key')
+        self.request = APIRequest(
+            'https://api.tineye.com/rest/',
+            'LCkn,2K7osVwkX95K4Oy',
+            '6mm60lsCNIB,FwOWjJqA80QZHh9BMwc-ber4u=t^')
 
     def tearDown(self):
         pass
@@ -62,33 +65,34 @@ class TestAPIRequest(unittest.TestCase):
         """
 
         signature = self.request._generate_hmac_signature('')
-        self.assertEquals(signature, '9e53b30cd6c6eb31c276ade9a29dcf5da7fd0f62')
+        self.assertEquals(signature, '882f24a1cbe144550c79aa37b77cc2fd2ac3cd1c')
 
         signature = self.request._generate_hmac_signature(' ')
-        self.assertEquals(signature, 'e84c818e22c0db649537f4015ee13efd34044366')
+        self.assertEquals(signature, 'e2652d19426f5904635f88fb374043acce49254f')
 
         signature = self.request._generate_hmac_signature('this is a message to convert')
-        self.assertEquals(signature, 'b373bc3be782f86d64421f0f3b1b394e9217f61d')
+        self.assertEquals(signature, 'b6ff6b77fc442ac679a2725a5e09d08271e01721')
 
         signature = self.request._generate_hmac_signature('this is another message to convert')
-        self.assertEquals(signature, 'fe0bdcae1763be1dd154c9141e03a956c25a068a')
+        self.assertEquals(signature, 'c7ec125f1e07cefd734d9c2f919deb5295a4e5f8')
 
         nonce = 'a_nonce'
         date = 1347910390
         signature = self.request._generate_get_hmac_signature('image_count', nonce, date)
-        self.assertEquals(signature, '75acee6ab58785fa04448df232e6f171cd4db819')
+        self.assertEquals(signature, '8e7bdc3195fce736922d6e622a6c68d8e4b43a55')
 
-        signature = self.request._generate_get_hmac_signature('remaining_searches', nonce, date, request_params={'param_1': 'value'})
-        self.assertEquals(signature, '7e6f8ff5c24d10e03d0c962f3d4a7e6df1ade352')
+        signature = self.request._generate_get_hmac_signature(
+            'remaining_searches', nonce, date, request_params={'param_1': 'value'})
+        self.assertEquals(signature, '25ea2472cfba2a64b81959f699fdca56537c6eb8')
 
         boundary = "--boundary!"
-        signature = self.request._generate_post_hmac_signature('search', boundary, nonce, date, 
-                                                               filename='file')
-        self.assertEquals(signature, 'caeae30f197efeca6766e546168bc942597dd7a5')
+        signature = self.request._generate_post_hmac_signature(
+            'search', boundary, nonce, date, filename='file')
+        self.assertEquals(signature, '7ed0ffdf83e3185d875d87c8b0f9645bfc1becc7')
 
-        signature = self.request._generate_post_hmac_signature('search', boundary, nonce, date, 
-                                                               filename='file', request_params={'param_1': 'value'})
-        self.assertEquals(signature, '0c5f2091631d4ad76207565b0cf9e9daf8e68465')
+        signature = self.request._generate_post_hmac_signature(
+            'search', boundary, nonce, date, filename='file', request_params={'param_1': 'value'})
+        self.assertEquals(signature, '0c661bc12f9319325d542475adcc5d975711597e')
 
     def test_sort_params(self):
         """ Test APIRequest._sort_params(). """
@@ -109,11 +113,19 @@ class TestAPIRequest(unittest.TestCase):
         sorted_params = self.request._sort_params(request_params)
         self.assertEquals(sorted_params, 'a_param=value_2&b_param=value_3&param_1=value_1')
 
-        request_params = {'api_key': 'a_key', 'param_1': 'value_1', 'a_param': 'value_2', 'b_param': 'value_3'}
+        request_params = {
+            'api_key': 'a_key',
+            'param_1': 'value_1',
+            'a_param': 'value_2',
+            'b_param': 'value_3'}
         sorted_params = self.request._sort_params(request_params)
         self.assertEquals(sorted_params, 'a_param=value_2&b_param=value_3&param_1=value_1')
 
-        request_params = {'api_key': 'a_key', 'param_1': 'value_1', 'api_sig': 'a_sig', 'a_param': 'value_2'}
+        request_params = {
+            'api_key': 'a_key',
+            'param_1': 'value_1',
+            'api_sig': 'a_sig',
+            'a_param': 'value_2'}
         sorted_params = self.request._sort_params(request_params)
         self.assertEquals(sorted_params, 'a_param=value_2&param_1=value_1')
 
@@ -144,10 +156,28 @@ class TestAPIRequest(unittest.TestCase):
         date = 1345821763
 
         url = self.request._request_url('search', nonce, date, 'api_sig', {'a_param': 'value_1'})
-        self.assertEquals(url, 'http://api.tineye.com/search/?api_key=public_key&date=1345821763&nonce=a_nonce&api_sig=api_sig&a_param=value_1')
+        self.assertEquals(
+            url,
+            ('https://api.tineye.com/rest/search/?api_key=LCkn,2K7osVwkX95K4Oy&date=1345821763'
+             '&nonce=a_nonce&api_sig=api_sig&a_param=value_1'))
 
-        url = self.request._request_url('search', nonce, date, 'api_sig', {'param_1': 'value_1', 'a_param': 'value_2', 'b_param': 'value_3'})
-        self.assertEquals(url, 'http://api.tineye.com/search/?api_key=public_key&date=1345821763&nonce=a_nonce&api_sig=api_sig&a_param=value_2&b_param=value_3&param_1=value_1')
+        url = self.request._request_url(
+            'search',
+            nonce,
+            date,
+            'api_sig',
+            {'param_1': 'value_1', 'a_param': 'value_2', 'b_param': 'value_3'})
+        self.assertEquals(
+            url,
+            ('https://api.tineye.com/rest/search/?api_key=LCkn,2K7osVwkX95K4Oy&date=1345821763'
+             '&nonce=a_nonce&api_sig=api_sig&a_param=value_2&b_param=value_3&param_1=value_1'))
 
-        url = self.request._request_url('search', nonce, date, 'api_sig', {'image_url': 'CAPS?!!?'})
-        self.assertEquals(url, 'http://api.tineye.com/search/?api_key=public_key&date=1345821763&nonce=a_nonce&api_sig=api_sig&image_url=caps%3f%21%21%3f')
+        url = self.request._request_url(
+            'search',
+            nonce, date,
+            'api_sig',
+            {'image_url': 'CAPS?!!?'})
+        self.assertEquals(
+            url,
+            ('https://api.tineye.com/rest/search/?api_key=LCkn,2K7osVwkX95K4Oy&date=1345821763'
+             '&nonce=a_nonce&api_sig=api_sig&image_url=CAPS%3F%21%21%3F'))
